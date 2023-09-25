8e529f415a68816519533c55062b0b6c57de42af
ViniciusIth <vini.ithalo@hotmail.com>
2023-09-24 21:31:24 -0300

    feat: Added bsreload command to reload config files

--- src/main/java/cloud/viniciusith/breakablespawner/BreakableSpawner.kt
+++ src/main/java/cloud/viniciusith/breakablespawner/BreakableSpawner.kt
@@ -1,11 +1,19 @@
 package cloud.viniciusith.breakablespawner
 
+import cloud.viniciusith.breakablespawner.commands.ReloadCommand
 import cloud.viniciusith.breakablespawner.listeners.SpawnerListener
 import org.bukkit.plugin.java.JavaPlugin
 
 class BreakableSpawner : JavaPlugin() {
     override fun onEnable() {
         saveDefaultConfig()
+
+        server.commandMap.register(
+            PLUGIN_NAME, ReloadCommand(
+                this, "reload", "Reloads Breakable Spawners configs", "/",
+                arrayListOf()
+            )
+        )
 
         server.pluginManager.registerEvents(SpawnerListener(this), this)
     }
